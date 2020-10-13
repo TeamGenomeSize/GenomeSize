55bb7069144c74645a38c3d34ebc8a5945ba7b7c
Ailin Zhang <z5207331@unsw.edu.au>
2020-10-13 16:21:04 +1100

    fixed name of .bai file

--- code/make_pileup.py
+++ code/make_pileup.py
@@ -15,7 +15,7 @@ try:
     # bamfile = "/srv/scratch/z3452659/BINF6112-Sep20/TeamGenomeSize/data/2020-09-22.ReferenceGenomes/e_coli/bam/e_coli.bam"
     bamfile = "/srv/scratch/{0}/e_coli.bam".format(zid)
     
-    samfile = pysam.AlignmentFile(bamfile, "rb", index_filename="/srv/scratch/{0}/ecoli.bam.bai".format(zid))
+    samfile = pysam.AlignmentFile(bamfile, "rb", index_filename="/srv/scratch/{0}/e_coli.bam.bai".format(zid))
     samfile._hasIndex()
     f = open("/srv/scratch/{0}/e_coli_ss.bed".format(zid), "r")
     outf = open("/srv/scratch/{0}/pysam.pileup.out".format(zid), "w")
