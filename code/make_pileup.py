123ffa03a97107d0e234fb03127270b023eda564
Ailin Zhang <z5207331@unsw.edu.au>
2020-10-14 13:38:23 +1100

    added two extra information lines in pileup output and sorted bed file

--- code/make_pileup.py
+++ code/make_pileup.py
@@ -17,7 +17,9 @@ try:
     
     samfile = pysam.AlignmentFile(bamfile, "rb", index_filename="/srv/scratch/{0}/e_coli.bam.bai".format(zid))
     # samfile._hasIndex()
-    f = open("/srv/scratch/{0}/e_coli_ss.bed".format(zid), "r")
+    
+    # sort -n -k 2 e_coli_ss.bed > e_coli_sorted.bed
+    f = open("/srv/scratch/{0}/e_coli_sorted.bed".format(zid), "r")
     outf = open("/srv/scratch/{0}/pysam.pileup.out".format(zid), "w")
 
     counts = []
@@ -32,34 +34,18 @@ try:
         
         for pileupcolumn in samfile.pileup(vals[0], int(vals[1]), int(vals[2])):
             outf.write("coverage at base {} : {}\n".format(pileupcolumn.reference_pos, pileupcolumn.nsegments))
-        
+            for pileupread in pileupcolumn.pileups:
+                if not pileupread.is_del and not pileupread.is_refskip:
+                    # query position is None if is_del or is_refskip is set.
+                    print ('\tbase in read %s = %s' %
+                          (pileupread.alignment.query_name,
+                           pileupread.alignment.query_sequence[pileupread.query_position]))
+                
         # counts.append(ncounts)
         
     samfile.close()
     f.close()
     outf.close()
-    
-    # error
-    # [E::idx_find_and_load] Could not retrieve index file for '/srv/scratch/z3452659/BINF6112-Sep20/TeamGenomeSize/data/2020-09-22.ReferenceGenomes/e_coli/bam/e_coli.bam'
-    # Traceback (most recent call last):
-    #   File "code/make_pileup.py", line 31, in <module>
-    #     for pileupcolumn in samfile.pileup(vals[0], int(vals[1]), int(vals[2])):
-    #   File "pysam/libcalignmentfile.pyx", line 1331, in pysam.libcalignmentfile.AlignmentFile.pileup
-    # ValueError: no index available for pileup
-
-    # copied from pysam documentation
-    
-    # for pileupcolumn in samfile.pileup("chr1", 100, 120):
-    # print("\ncoverage at base %s = %s" %
-    #        (pileupcolumn.pos, pileupcolumn.n))
-    # for pileupread in pileupcolumn.pileups:
-    #     if not pileupread.is_del and not pileupread.is_refskip:
-    #         # query position is None if is_del or is_refskip is set.
-    #         print ('\tbase in read %s = %s' %
-    #               (pileupread.alignment.query_name,
-    #                pileupread.alignment.query_sequence[pileupread.query_position]))
-    # samfile.close()
-    
 
 
 except FileNotFoundError:
