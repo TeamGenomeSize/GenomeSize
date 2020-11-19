cbc297dfaacd964514d932381cbe56949a8e7fd7
Ailin Zhang <z5207331@unsw.edu.au>
2020-11-19 23:32:24 +1100

    added statistics module because math does not have mode apparently

--- code/getDepth.py
+++ code/getDepth.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import math
+import statistics
 import time
 from pathlib import Path
 
@@ -151,7 +152,7 @@ def medMedian(depths):
     return maxes[math.floor(len(maxes)/2)]
     
 def modeDepth(depths):
-    return math.mode(depths)
+    return statistics.mode(depths)
         
 if __name__ == "__main__":
     usageExample()
