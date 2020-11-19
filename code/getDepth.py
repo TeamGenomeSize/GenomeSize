3cd89ad075010c834b8808fa16eca0da41543f65
Ailin Zhang <z5207331@unsw.edu.au>
2020-11-19 22:48:29 +1100

    renamed incorrect variable

--- code/getDepth.py
+++ code/getDepth.py
@@ -151,7 +151,7 @@ def medMedian(depths):
     return maxes[math.floor(len(maxes)/2)]
     
 def modeDepth(depths):
-    return mode(allDepths)
+    return mode(depths)
         
 if __name__ == "__main__":
     usageExample()
